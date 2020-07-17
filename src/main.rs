ad291050aea2c23a93ca1399de0351603aef9568
Peter PJT <pjtsignups@gmail.com>
2020-07-17 11:54:50 -0700

    "feat(cli): start adding cli"

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate yaml_rust;
+extern crate clap;
+use clap::{App, load_yaml};
 use std::path::PathBuf;
 use crate::pkg::Pkg;
 use yaml_rust::{YamlLoader};
@@ -9,12 +11,19 @@ mod traits;
 mod utils;
 
 fn main() {
-    let yaml_str:&str = &fs::read_to_string("./pkg.yaml").expect("Could not read pkg.yaml");
+    let cli = load_yaml!("./cli.yaml");
+    let matches = App::from_yaml(cli).get_matches();
+    let input = PathBuf::from(matches.value_of("INPUT").unwrap_or("./pkg.yaml"));
+    let working_dir = PathBuf::from(matches.value_of("working_dir").unwrap_or("./tmp"));
+
+    let yaml_str:&str = &fs::read_to_string(input).expect("Could not read pkg.yaml");
     let yaml = &YamlLoader::load_from_str(yaml_str).expect("Could not parse pkg")[0];
     let mut pkg = Pkg::from(yaml);
-    install(&mut pkg, Some(PathBuf::from("./tmp")))
+    if let Some(_matches) = matches.subcommand_matches("install") {
+        install(&mut pkg, working_dir);
+    }
 }
 
-fn install(pkg:&mut Pkg, working_dir:Option<PathBuf>) {
-    pkg.install(working_dir.unwrap_or(PathBuf::from("./tmp"))).unwrap();
+fn install(pkg:&mut Pkg, working_dir:PathBuf) {
+    pkg.install(working_dir).unwrap();
 }
